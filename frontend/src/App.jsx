9baa366d88b50b21a8a5b29043d81389ee87af96
Muizz Ahmed <mmdmuizzahmed.09.a@gmail.com>
2026-02-04 19:25:48 +0530

    initial frontend and backend setup - installed oracledb and express dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+    return(
+        <div className="flex justify-center items-center min-h-screen bg-gray-900">
+            <h2 className="text-4xl font-mono text-amber-600">Initial setup for DBS project</h2>
+        </div>
+    )
+}
+
+export default App
